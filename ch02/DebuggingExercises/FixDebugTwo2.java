01a247ea94147ce73997522af1610e0b56b90835
wangchaohsuan <chaohsuan.wang@my.jcu.edu.au>
2021-03-23 10:33:09 +0800

    fix error

---
+++ ch02/DebuggingExercises/FixDebugTwo2.java
@@ -0,0 +1,14 @@
+public class FixDebugTwo2
+//  This application performs arithmetic with two integers
+{
+   public static void main(String[] args)
+   {
+      int a, b, d;
+      a = 7;
+      b = 4;
+      d = a-b;
+      System.out.println("The sum is " + a + b);
+      System.out.println("The difference is " + d);
+      System.out.println("The product is " + d);
+   }
+}
