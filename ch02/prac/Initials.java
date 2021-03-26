ee73b6d56a2664c182c30618788327d4237bbd22
wangchaohsuan <chaohsuan.wang@my.jcu.edu.au>
2021-03-27 00:28:33 +0800

    add Initials

---
+++ ch02/prac/Initials.java
@@ -0,0 +1,9 @@
+public class Initials {
+    public static void main(String[] args) {
+        char firstInitial = 'J';
+        char secondInitial = 'M';
+        char thirdInitial = 'F';
+        System.out.println(firstInitial+"."+secondInitial+"."+thirdInitial+".");
+    }
+
+}
